9c53d3a7629fd9b21df88110a2ed3e01f47f8fe3
danderson <danielmckinnon.anderson@gmail.com>
2021-04-01 14:59:08 -0500

    Update StaffRepository
    Added method necessary for patient search ( find Staff with title 'doctor')

--- src/main/java/com/simpleschedule/daniel/anderson/repositories/StaffRepository.java
+++ src/main/java/com/simpleschedule/daniel/anderson/repositories/StaffRepository.java
@@ -1,9 +1,11 @@
 package com.simpleschedule.daniel.anderson.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.simpleschedule.daniel.anderson.entities.Staff;
 
 public interface StaffRepository extends CrudRepository<Staff, Integer>{
-
+	List<Staff> findByTitle(String sTitle);
 }
