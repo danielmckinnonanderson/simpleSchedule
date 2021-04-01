b36e7910131b14b8b3888cdd0dda8538ece29b1f
danderson <danielmckinnon.anderson@gmail.com>
2021-04-01 14:34:53 -0500

    Create PatientRepository methods
    create abstract query methods for searching for a patient in the PatientRepository class

--- src/main/java/com/simpleschedule/daniel/anderson/repositories/PatientRepository.java
+++ src/main/java/com/simpleschedule/daniel/anderson/repositories/PatientRepository.java
@@ -9,23 +9,31 @@ import com.simpleschedule.daniel.anderson.entities.Patient;
 public interface PatientRepository extends CrudRepository<Patient, Integer>{
 	
 	//CUSTOM FINDER METHODS FOR ATTRIBUTES
-	public Patient findByPLastName(String pLastName);
-	
-	public Patient findByPLastNameAndPFirstName(String pLastName, String pFirstName);
-	
-	public Patient findByPLastNameAndPFirstNameAndPDob(
-			String pLastName, String pFirstnName, Date pDob);
-	
-	public Patient findByPLastNameAndPFirstNameAndPPrimary(
-			String pLastName, String pFirstName, Integer pPrimary);
-	
-	public Patient findByPFirstNameAndPLastNameAndPDobAndPPrimaryId(
-			String pFirstName, String pLastName, Date pDob, Integer pPrimaryId);
+	//ALL POSSIBLE PERMUTATIONS
 	
 	public Patient findByPFirstName(String pFirstName);
+	public Patient findByPFirstNameAndPLastName(String pFirstName, String pLastName);
+	public Patient findByPFirstNameAndPDob(String pFirstName, Date pDob);
+	public Patient findByPFirstNameAndPPrimary(String pFirstName, Integer pPrimary);
 	
+	public Patient findByPFirstNameAndPLastNameAndPDob(String pFirstName, String pLastName, Date pDob);
+	public Patient findByPFirstNameAndPLastNameAndPPrimary(
+			String pFirstName, String pLastName, Integer pPrimary);
+	public Patient findByPFirstNameAndPDobAndPPrimary(String pFirstName, Date pDob, Integer pPrimary);
+		
+	
+	public Patient findByPLastName(String pLastName);
+	public Patient findByPLastNameAndPDob(String pLastName, Date pDob);
+	public Patient findByPLastNameAndPPrimary(String pLastName, Integer pPrimary);
+	
+	public Patient findByPLastNameAndPDobAndPPrimary(String pLastName, Date pDob, Integer pPrimary);
+	
+
 	public Patient findByPDob(Date pDob);
+	public Patient findByPDobAndPPrimary(Date pDob, Integer pPrimary);
 	
 	public Patient findByPPrimary(Integer pPrimary);
 	
+	public Patient findByPFirstNameAndPLastNameAndPDobAndPPrimaryId(
+			String pFirstName, String pLastName, Date pDob, Integer pPrimaryId);	
 }
