f73537f2c9cb5295f0a8a367c43148fbdd1deaff
danderson <danielmckinnon.anderson@gmail.com>
2021-04-01 20:18:46 -0500

    Update NavigationController
    corrected errors in constructor causing failure to launch correctly

--- src/main/java/com/simpleschedule/daniel/anderson/controllers/NavigationController.java
+++ src/main/java/com/simpleschedule/daniel/anderson/controllers/NavigationController.java
@@ -1,14 +1,20 @@
 package com.simpleschedule.daniel.anderson.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.simpleschedule.daniel.anderson.entities.Patient;
+import com.simpleschedule.daniel.anderson.services.StaffService;
 
 @Controller
 public class NavigationController {
+	private StaffService staffService;
 
 	
-	public NavigationController() {
+	public NavigationController(StaffService staffService) {
 		super();
+		this.staffService = staffService;
 	}
 	
 	@GetMapping("/")
@@ -17,7 +23,8 @@ public class NavigationController {
 	}
 	
 	@GetMapping("/patient_search")
-	public String showPatientSearch() {
+	public String showPatientSearch(Model model) {
+		model.addAttribute("doctorList", staffService.findAllDoctors());
 		return "patient_search";
 	}
 	
