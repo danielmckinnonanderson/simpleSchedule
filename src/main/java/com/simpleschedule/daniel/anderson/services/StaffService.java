0f42725b56f227f9c03914ff9973aa5371c4df3b
danderson <danielmckinnon.anderson@gmail.com>
2021-04-01 20:18:03 -0500

    Update StaffService
    add required Bean annotations

--- src/main/java/com/simpleschedule/daniel/anderson/services/StaffService.java
+++ src/main/java/com/simpleschedule/daniel/anderson/services/StaffService.java
@@ -2,13 +2,22 @@ package com.simpleschedule.daniel.anderson.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.simpleschedule.daniel.anderson.entities.Staff;
 import com.simpleschedule.daniel.anderson.repositories.StaffRepository;
 
+@Service
 public class StaffService {
 	private StaffRepository staffRepository;
 	
-	public List<Staff> findAllDoctors(String sTitle) {
-		return staffRepository.findByTitle("Doctor");
+	@Autowired
+	public StaffService(StaffRepository staffRepository) {
+		this.staffRepository = staffRepository;
+	}
+	
+	public List<Staff> findAllDoctors() {
+		return staffRepository.findBySTitle("Doctor");
 	}
 }
