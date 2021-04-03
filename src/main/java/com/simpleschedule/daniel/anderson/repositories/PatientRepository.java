82a44cb9ec838a8e32e1ab9add8738cef8e1267f
danderson <danielmckinnon.anderson@gmail.com>
2021-04-03 16:44:20 -0500

    update PatientRepository
    corrected Date datatype from java.sql.Date to java.util.Date

--- src/main/java/com/simpleschedule/daniel/anderson/repositories/PatientRepository.java
+++ src/main/java/com/simpleschedule/daniel/anderson/repositories/PatientRepository.java
@@ -1,6 +1,6 @@
 package com.simpleschedule.daniel.anderson.repositories;
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
