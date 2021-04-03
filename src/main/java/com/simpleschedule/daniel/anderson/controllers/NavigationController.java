5724d4dbed25c854b1c3c22923a9c86fe296f3bb
danderson <danielmckinnon.anderson@gmail.com>
2021-04-03 16:38:14 -0500

    Update NavigationController
    Added missing @Autowired annotation on NavigationController, moved methods related to patient_search functionality to new PatientSearchController to keep things separated and clean organizationally.

--- src/main/java/com/simpleschedule/daniel/anderson/controllers/NavigationController.java
+++ src/main/java/com/simpleschedule/daniel/anderson/controllers/NavigationController.java
@@ -1,43 +1,33 @@
 package com.simpleschedule.daniel.anderson.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import com.simpleschedule.daniel.anderson.entities.Patient;
-import com.simpleschedule.daniel.anderson.services.StaffService;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NavigationController {
-	private StaffService staffService;
 
-	
-	public NavigationController(StaffService staffService) {
+	@Autowired
+	public NavigationController() {
 		super();
-		this.staffService = staffService;
 	}
-	
+
 	@GetMapping("/")
 	public String showMainPage() {
 		return "index";
 	}
-	
-	@GetMapping("/patient_search")
-	public String showPatientSearch(Model model) {
-		model.addAttribute("doctorList", staffService.findAllDoctors());
-		return "patient_search";
-	}
-	
+
 	@GetMapping("/patient_add")
 	public String showPatientAdd() {
 		return "patient_add";
 	}
-	
+
 	@GetMapping("/appointment_search")
 	public String showAppointmentSearch() {
 		return "appointment_search";
 	}
-	
+
 	@GetMapping("/appointment_add")
 	public String showAppointmentAdd() {
 		return "appointment_add";
