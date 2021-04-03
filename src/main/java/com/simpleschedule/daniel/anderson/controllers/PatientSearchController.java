3e5cbca77f7a5b5c22608087880cc91ad409b027
danderson <danielmckinnon.anderson@gmail.com>
2021-04-03 16:39:26 -0500

    Create PatientSearchController
    create new Controller to handle all operations related specifically to patient_search functionality. Added necessary request methods for search form submission, processing form fields, and displaying the result list on a new page

---
+++ src/main/java/com/simpleschedule/daniel/anderson/controllers/PatientSearchController.java
@@ -0,0 +1,59 @@
+package com.simpleschedule.daniel.anderson.controllers;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.simpleschedule.daniel.anderson.entities.Patient;
+import com.simpleschedule.daniel.anderson.entities.Staff;
+import com.simpleschedule.daniel.anderson.services.PatientService;
+import com.simpleschedule.daniel.anderson.services.StaffService;
+
+@Controller
+public class PatientSearchController {
+	private PatientService patientService;
+	private StaffService staffService;
+	
+	@Autowired
+	public PatientSearchController(PatientService patientService, StaffService staffService) {
+		this.patientService = patientService;
+		this.staffService = staffService;
+	}
+	
+	@GetMapping("/patient_search")
+	public String showPatientSearch(Model model, HttpSession session) {
+		if (session.getAttribute("doctorList") == null) {
+			List<Staff> doctorList = staffService.findAllDoctors();
+			session.setAttribute("doctorList", doctorList);
+		}	
+		model.addAttribute("searchPatient", new Patient());
+		return "patient_search";
+	}
+	
+	
+	@PostMapping("/patient_search")
+	public String processPatientSearch(
+			@ModelAttribute("searchPatient") Patient searchPatient,
+			BindingResult result,
+			Model model) {
+		if (result.hasErrors()) {
+			return "patient_search";
+		}
+
+		List<Patient> resultsList = patientService.findPatientUsingFields(searchPatient);
+		model.addAttribute("resultsList", resultsList);
+		
+		return "search_results";
+	}
+}
