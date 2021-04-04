ca4d1c281ac2ffd722d31274792686a8a79a9c0c
danderson <danielmckinnon.anderson@gmail.com>
2021-04-04 16:30:45 -0500

    Create ContactService
    Create service layer to interact with ContactRepository

---
+++ src/main/java/com/simpleschedule/daniel/anderson/services/ContactService.java
@@ -0,0 +1,22 @@
+package com.simpleschedule.daniel.anderson.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.simpleschedule.daniel.anderson.entities.Contact;
+import com.simpleschedule.daniel.anderson.repositories.ContactRepository;
+
+@Service
+public class ContactService {
+	private ContactRepository contactRepository;
+	
+	@Autowired
+	ContactService(ContactRepository contactRepository) {
+		this.contactRepository = contactRepository;
+	}
+	
+	//SAVE NEW CONTACT
+	public Contact saveNewContact(Contact contact) {
+		return contactRepository.save(contact);
+	}
+}
