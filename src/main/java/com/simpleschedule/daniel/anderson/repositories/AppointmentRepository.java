011234257761ec7eb6ec966b03731614e19edec3
danderson <danielmckinnon.anderson@gmail.com>
2021-04-07 13:19:01 -0500

    Update AppointmentRepository
    added basic abstract query methods

--- src/main/java/com/simpleschedule/daniel/anderson/repositories/AppointmentRepository.java
+++ src/main/java/com/simpleschedule/daniel/anderson/repositories/AppointmentRepository.java
@@ -1,9 +1,20 @@
 package com.simpleschedule.daniel.anderson.repositories;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.simpleschedule.daniel.anderson.entities.Appointment;
 
 public interface AppointmentRepository extends CrudRepository<Appointment, Integer> {
-
+	//FIND APPOINTMENTS BY COLUMN
+	Appointment findByAId(Integer aId);
+	List<Appointment> findByaPatientId(Integer aPatientId);
+//	List<Appointment> findByADateByOrderByATimeStart(Date aDate);
+//	List<Appointment> findByaPrimaryIdByOrderByADate(Integer aPrimaryId);
+	
+	//FIND APPOINTMENTS BY COMBINTATION OF COLUMNS
+	List<Appointment> findByADateAndATimeStart(Date aDate, LocalDateTime aTimeStart);
 }
