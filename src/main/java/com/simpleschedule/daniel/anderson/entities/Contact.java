106fa0db5778f8fcac68435ea81e1a3655fcd493
danderson <danielmckinnon.anderson@gmail.com>
2021-04-09 14:43:11 -0500

    Update Contact
    add overriding equals() method

--- src/main/java/com/simpleschedule/daniel/anderson/entities/Contact.java
+++ src/main/java/com/simpleschedule/daniel/anderson/entities/Contact.java
@@ -60,6 +60,43 @@ public class Contact {
 	}
 
 	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contact other = (Contact) obj;
+		if (cEmail == null) {
+			if (other.cEmail != null)
+				return false;
+		} else if (!cEmail.equals(other.cEmail))
+			return false;
+		if (cPatientId == null) {
+			if (other.cPatientId != null)
+				return false;
+		} else if (!cPatientId.equals(other.cPatientId))
+			return false;
+		if (cPhone1 == null) {
+			if (other.cPhone1 != null)
+				return false;
+		} else if (!cPhone1.equals(other.cPhone1))
+			return false;
+		if (cPhone2 == null) {
+			if (other.cPhone2 != null)
+				return false;
+		} else if (!cPhone2.equals(other.cPhone2))
+			return false;
+		if (cPhone3 == null) {
+			if (other.cPhone3 != null)
+				return false;
+		} else if (!cPhone3.equals(other.cPhone3))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return String.format("Contact for Patient ID %d: Phone 1- %d, Phone 2- %d, Phone 3-%d, Email- %s",
 				cPatientId, cPhone1, cPhone2, cPhone3, cEmail);
